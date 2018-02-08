d8651489b7ed58ce513f91d7ffd70ebf9cc387ee
Blagica Stojanoska <blagicastojanoska@gmail.com>
2018-02-08 18:19:10 +0100

    one vid

--- models/Store.js
+++ models/Store.js
@@ -37,7 +37,10 @@ const storeSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         required: 'You must supply an author!'
-    }
+    }, 
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
 // Define our indexes
@@ -71,6 +74,13 @@ storeSchema.statics.getTagsList = function() {
         { $group: { _id: '$tags', count: { $sum: 1 } } },
         { $sort: { count: -1 } }
     ]);
-}
+};
+
+// find reviews where the store _id property === reviews store property
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link?
+    localField: '_id', // which field on the store?
+    foreignField: 'store' // which field on the review?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
